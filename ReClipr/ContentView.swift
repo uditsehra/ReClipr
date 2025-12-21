2c92e1153744d6ce391ab1c7b9829eb2c8375c2e
Udit Sehra <uditsehra@gmail.com>
2025-12-21 16:18:11 +0530

    Initial Commit

---
+++ ReClipr/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ReClipr
+//
+//  Created by Udit Sehra on 21/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ReClipr/ReCliprApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReCliprApp.swift
+//  ReClipr
+//
+//  Created by Udit Sehra on 21/12/25.
+//
+
+import SwiftUI
+
+@main
+struct ReCliprApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
